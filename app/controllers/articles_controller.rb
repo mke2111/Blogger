ada8a7afa4ef3c3e5f066f9548ab362026d5f7a4
Marcelo Fernandes de Araujo <marcelomaidden@gmail.com>
2020-11-11 13:55:13 -0300

    Add update and edit method using partials

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -26,4 +26,15 @@ class ArticlesController < ApplicationController
 
     redirect_to action: "index"
   end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+  
+    redirect_to article_path(@article)
+  end
 end
