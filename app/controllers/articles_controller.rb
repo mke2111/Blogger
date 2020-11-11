041674d7c702b86a44e8a92c49821ba96903585c
Marcelo Fernandes de Araujo <marcelomaidden@gmail.com>
2020-11-11 13:34:23 -0300

    Add helper to articles save

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  include ArticlesHelper
+
   def index
     @articles = Article.all
   end
@@ -12,8 +14,7 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new
-    @article.title = params[:article][:title]
+    @article = Article.new(article_params)
     @article.save
     redirect_to article_path(@article)
 
